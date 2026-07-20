99ff43a701892023e02b10d3b287259a370eea40
SanjeevSinjali <tdkzbiru@gmail.com>
2026-07-20 20:38:24 +0545

    Added Notification Model

---
+++ kitabghar_backend/src/models/notification.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface INotification extends Document {
+  type: string;
+  message: string;
+  user: mongoose.Types.ObjectId;
+  changedFields: string[];
+  read: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const NotificationSchema = new Schema<INotification>(
+  {
+    type: { type: String, required: true, default: "profile_update" },
+    message: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    changedFields: { type: [String], default: [] },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<INotification>("Notification", NotificationSchema);
