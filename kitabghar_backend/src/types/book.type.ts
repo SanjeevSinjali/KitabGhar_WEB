586c505180e47646edca4f4a46932005f9fcf8b0
SanjeevSinjali <tdkzbiru@gmail.com>
2026-07-20 22:49:27 +0545

    add book validation schema

---
+++ kitabghar_backend/src/types/book.type.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+export type { CreateBookDTO } from "../dtos/book.dto";
+
+export const CreateBookSchema = z.object({
+  title: z.string().min(2, "Title must be at least 2 characters"),
+  author: z.string().min(2, "Author must be at least 2 characters"),
+  price: z.coerce.number().positive("Price must be greater than 0"),
+  condition: z.enum(["Like New", "Good", "Fair"]).default("Good"),
+  description: z.string().max(500, "Description is too long").optional(),
+});
