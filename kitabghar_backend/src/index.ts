46afdb7fbbe62b7e58b0f8385b230cefd655c840
SanjeevSinjali <tdkzbiru@gmail.com>
2026-07-20 22:52:25 +0545

    mount book routes and serve book images

--- kitabghar_backend/src/index.ts
+++ kitabghar_backend/src/index.ts
@@ -8,6 +8,8 @@ import { connectDB } from "./database/mongodb";
 import userRoutes from "./routes/user.route";
 import adminUserRoutes from "./routes/admin/user.route";
 import adminNotificationRoutes from "./routes/admin/notification.route";
+import adminBookRoutes from "./routes/admin/book.route";
+import bookRoutes from "./routes/book.route";
 
 dotenv.config();
 
@@ -23,12 +25,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/avatars", express.static(path.join(__dirname, "../public/avatars")));
+app.use("/books", express.static(path.join(__dirname, "../public/books")));
 
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/admin/users", adminUserRoutes);
-
-app.use("/api/v1/admin/users", adminUserRoutes);
 app.use("/api/v1/admin/notifications", adminNotificationRoutes);
+app.use("/api/v1/admin/books", adminBookRoutes);
+app.use("/api/v1/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "KitabGhar API is running" });
